0d9deca2899d9c657d7e433025ecdc5d0fc48f98
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2021-03-11 22:19:33 +0530

    changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ export default function Welcome({ name }) {
   return (
     <>
       <h1>Hey !{name} </h1>
-      <h2>Welcome to Newton School</h2>
+      <h2>Welcome to Newton School.</h2>
     </>
   );
 }
